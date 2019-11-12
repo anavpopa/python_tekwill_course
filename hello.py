c6f4b2c9a98c27447d9897e3c90251ec50f4beec
Ana Popa <anavpopa@gmail.com>
2019-11-12 19:49:43 +0200

    hello.py

---
+++ hello.py
@@ -0,0 +1,20 @@
+a = ['a', 'b', 'c', 'd']
+a[int(int('3'*2))//11]
+print(a[int(int('3'*2))//11])
+
+a = ['a', 'b', 'c', 'd']
+a.insert(0,7)
+print(a)
+
+a = ['a', 'b', 'c', 'd']
+a=a[0:-2]
+print(a)
+
+a = ['a', 'b', 'c', 'd']
+a.sort(reverse=True)
+print(a)
+
+a = ['a', 'b', 'c', 'd']
+b=a[2:]
+print(b)
+
