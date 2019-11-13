1c9ba70809803eb8f4c0b55b30fab7cea2e1eb58
Ana Popa <48568597+anavpopa@users.noreply.github.com>
2019-11-13 23:43:11 +0200

    Lesson4

---
+++ Lesson4_Ex1(set2).py
@@ -0,0 +1,15 @@
+str=input('Introduceti textul')
+digit=0
+letter=0
+i=0
+for i in range(len(str)):
+    if str[i].isnumeric():
+        digit=digit+1
+        i=i+1
+    elif str[i].isalpha():
+        letter=letter+1
+        i=i+1
+    else:
+        i=i+1
+print(digit)
+print(letter)
