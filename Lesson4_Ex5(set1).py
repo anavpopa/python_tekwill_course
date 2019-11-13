99f1ceea21aa39b7980397b8c028b0c05906abca
Ana Popa <48568597+anavpopa@users.noreply.github.com>
2019-11-14 00:26:08 +0200

    Delete Lesson4_Ex5(set1).py

--- Lesson4_Ex5(set1).py
+++
@@ -1,8 +0,0 @@
-#Exercitiul 5 (Calculati suma toturor numerelor intre 1000 si 2300 care se impart fara rest la 5 si 7)
-sum=0
-n=1000
-while n<=2300:
-    if n%5==0 and n%7==0:
-        sum=sum+n
-    n=n+1
-print(sum)
